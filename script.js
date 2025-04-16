79e3f09264a1affc581ce50fd2540ddb413c26fe
Muhammad Usama <em.usama2004@gmail.com>
2025-04-16 15:25:43 +0500

    sending data to local storage

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ let showPasswordButton = document.querySelector("#showPassword");
 let showConfirmPasswordButton = document.querySelector("#showConfirmPassword");
 let submit = document.querySelector("#btn");
 let paraName = document.getElementById("paraname");
-let paraEmail = document.getElementById("email");
-let paraPassword = document.getElementById("password");
-let paraConfirmPassword = document.getElementById("confirmPassword");
+let paraEmail = document.getElementById("paraemail");
+let paraPassword = document.getElementById("parapassword");
+let paraConfirmPassword = document.getElementById("paraconfirmPassword");
 let Address = document.getElementById("address");
 let paraAddress = document.getElementById("paraaddress");
-let postalAddress = document.getElementById("postaladdres");
+let postalAddress = document.getElementById("postaladdress");
 let paraPostalAddress = document.getElementById("postaddress");
 let paraPhone = document.getElementById("number");
+let parts = Email.value.split(".");
+    let extension = parts[parts.length - 1];
 
 showPasswordButton.addEventListener("click", function () {
     if (Password.type === "password") {
@@ -38,71 +40,102 @@ submit.addEventListener("click",function(event){
 
     if (Name.value === ""){
         paraName.innerText = "Please enter your Name"
-        return
+        
     }
     if(Email.value === ""){
         paraEmail.innerText = "Please Enter Your Email"
-        return
+        
     }
     if (!Email.value.includes("@")) {
         paraEmail.innerText = "Email must contain '@'."
-        return;
+        
     }
-    let parts = Email.value.split(".");
-    let extension = parts[parts.length - 1];
+    
 
     if (extension.length > 3) {
         paraEmail.innerText = "Email extension must be 3 characters or less."
-        return;
+        
     }
-    if(Password.value.length < 8){
+     if(Password.value.length < 8){
         paraPassword.innerText = "password should be 8 character or more"
-        return
+        
     }
     // let password = Password.value;
 
     if (!/(?=.*[a-z])/.test(Password.value)) {
         paraPassword.innerText = "Password must contain at least one lowercase letter."
-        return;  
+          
     }
       
       
     if (!/(?=.*[A-Z])/.test(Password.value)) {
         paraPassword.innerText = "Password must contain at least one uppercase letter."
-        return;
+        
     }
       
       
     if (!/(?=.*\d)/.test(Password.value)) {
         paraPassword.innerText = "Password must contain at least one number."
-        return;
+        
     }
       
       
     if (!/(?=.*[\W_])/.test(Password.value)) {
         paraPassword.innerText = "Password must contain at least one special character (e.g., !@#$%^&*)."
-        return;
+        
     }
     
     
     
     if(Password.value !== ConfirmPassword.value){
         paraConfirmPassword.innerText = "Password does not matches"
-        return
+        
     }
     if(Address.value === ""){
         paraAddress.innerText = "Please enter Address";
-        return
+        
     }
     if(postalAddress.value === ""){
         paraPostalAddress.innerText = "Enter Your Postal Address";
     }
+    if(PhoneNumber.value === ""){
+        paraPhone.innerText = "Must provide Your Number"
+    }
     if(PhoneNumber.value.length > 11 || PhoneNumber.value.length < 11){
         paraPhone.innerText = "Phone Number should be 11 digits"
-        return
+        
     }
-    if (/(\d).*\1/.test(PhoneNumber.value)) {
-        paraPhone.innerText = "Phone number must contain different digits (no repeating digits)."
-        return;
-    };
-})
+    // if (/(\d).*\1/.test(PhoneNumber.value)) {
+    //     paraPhone.innerText = "Phone number must contain different digits (no repeating digits)."
+    
+    // };
+    
+ console.log(Name.value)
+
+let userdata = {
+    user_name : Name.value,
+    user_email : Email.value,
+    user_password : Password.value,
+    user_confirmpassword : ConfirmPassword.value,
+    user_phonenumber : PhoneNumber.value,
+    user_address : Address.value,
+    user_postaladdress : postalAddress.value
+}
+
+let exist_data = localStorage.getItem("users");
+let userarray = exist_data ? JSON.parse(exist_data) : [];
+userarray.push(userdata);
+localStorage.setItem("users",JSON.stringify(userarray));
+console.log("user data saved",userarray);
+// let convert = JSON.stringify(userdata);
+// JSON.stringify(user_array.push(localStorage.setItem("user",convert)));
+// console.log(user_array)
+
+    // console.log(Name.value);
+    // console.log(Email.value);
+    // console.log(Password.value);
+    // console.log(ConfirmPassword.value);
+    // console.log(PhoneNumber.value);
+    // console.log(Address.value);
+    // console.log(postalAddress.value);
+});
